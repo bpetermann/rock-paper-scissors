5497b19821a4e91a322a7d3cbb770a1468d1c666
Benjamin Petermann <benjamin.petermann@gmx.at>
2021-06-01 21:28:08 +0200

    Script.js Update

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 
-let computerSelection
-let playerSelection
+let computerSelection;
+let playerSelection;
+let yourCount = 0;
+let cpuCount = 0;
+
+function game(){
+
 
 function playRound() {
 
@@ -8,7 +13,7 @@ function computerPlay() {
   let choice = ["Rock", "Paper", "Scissors"]
   return choice[Math.floor(Math.random() *choice.length)]
     } 
-    
+
   computerSelection = computerPlay();
   playerSelection = prompt ("Rock, Paper, Scissors?");
 
@@ -16,24 +21,30 @@ function computerPlay() {
     return ("Draw! Rock against Rock");
   } 
   else if (computerSelection  == "Paper" && playerSelection.toLowerCase() == "rock") {
+    cpuCount++;
     return ("You Lose! Paper beats Rock");
   } 
   else if (computerSelection == "Scissors" && playerSelection.toLowerCase() == "rock") {
+    yourCount++;
     return ("You Win! Rock beats Scissors");
   } 
   else if (computerSelection == "Rock" && playerSelection.toLowerCase() == "paper") {
+    yourCount++;
     return ("You Win! Paper beats Rock");
   } 
   else if (computerSelection == "Paper" && playerSelection.toLowerCase() == "paper") {
     return ("Draw! Paper against Paper");
   } 
   else if (computerSelection == "Scissors" && playerSelection.toLowerCase() == "paper") {
+    cpuCount++;
     return ("You Lose! Scissor beats Paper");
   } 
   else if (computerSelection == "Rock" && playerSelection.toLowerCase() == "scissors") {
+    cpuCount++;
     return ("You Lose! Rock beats Scissors");
   } 
   else if (computerSelection == "Paper" && playerSelection.toLowerCase() == "scissors") {
+    yourCount++;
     return ("You Win! Scissors beats Paper");
   } 
   else if (computerSelection == "Scissors" && playerSelection.toLowerCase() == "scissors") {
@@ -42,3 +53,26 @@ function computerPlay() {
 }
 
 console.log (playRound(playerSelection, computerSelection));
+console.log (playRound(playerSelection, computerSelection));
+console.log (playRound(playerSelection, computerSelection));
+console.log (playRound(playerSelection, computerSelection));
+console.log (playRound(playerSelection, computerSelection));
+
+
+if (yourCount >= 3){
+  console.log ("You Win!")
+  alert ("You Win!")
+}
+else if(cpuCount >= 3)
+{
+  console.log ("You Lose!")
+  alert ("You Lose!")
+}
+else {
+  console.log ("Draw! Nobody Wins")
+  alert ("Draw! Nobody Wins")
+}
+
+}
+
+console.log (game())
